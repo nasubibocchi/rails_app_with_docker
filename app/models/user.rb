c8d09571286eb61e6d4a5fc43a67d32068163b46
nasubibocchi <matsuhito125@gmail.com>
2024-02-04 20:43:28 +0900

    Implement annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
@@ -9,4 +24,6 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
                     # DBレベルでのユニーク制約はindex追加（add_index_to_users_email）で定義する必要がある
+
+  has_secure_password
 end
