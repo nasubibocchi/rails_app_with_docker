80bfb31d4cf80cd55e7a1e4333e8a322be07f8a1
nasubibocchi <matsuhito125@gmail.com>
2024-02-04 20:23:32 +0900

    Add email lower-case test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
