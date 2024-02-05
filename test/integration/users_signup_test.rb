be7add1f04302bf75708a5ae66ef755dca1b0355
nasubibocchi <matsuhito125@gmail.com>
2024-02-05 14:07:35 +0900

    Check post url from user form

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,12 +5,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
 
     assert_no_difference 'User.count' do
+      assert_select 'form[action="/signup"]'
       ## assert_no_difference 'User.count'と post users_path で実行される処理
       # before_count = User.count
       # post users_path, ...
       # after_count  = User.count
       # assert_equal before_count, after_count
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
@@ -23,7 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
 
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      assert_select 'form[action="/signup"]'
+      post signup_path, params: { user: { name:  "",
                                          email: "user@example.com",
                                          password:              "foobar",
                                          password_confirmation: "foobar" } }
