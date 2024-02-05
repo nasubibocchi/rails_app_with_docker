d2dcf52f076c3f6ad635b06a898344b3d022bfde
nasubibocchi <matsuhito125@gmail.com>
2024-02-05 14:58:03 +0900

    Separate test as valid sign up situation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,15 +18,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     # users/new ページにとどまることを確認
     assert_template 'users/new'
+  end
 
+  test "valid signup information" do
+    get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
-                                 email: "user@example.com",
-                                 password:              "password",
-                                 password_confirmation: "password" } }
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     follow_redirect!
-    # users/show ページに移動することを確認
     assert_template 'users/show'
     assert_not flash.empty?
   end
