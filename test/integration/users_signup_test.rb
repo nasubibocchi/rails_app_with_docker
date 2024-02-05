e348394add3c8f2918b50c01e8f647806d2c68d0
nasubibocchi <matsuhito125@gmail.com>
2024-02-05 12:58:30 +0900

    Create error test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      ## assert_no_difference 'User.count'と post users_path で実行される処理
+      # before_count = User.count
+      # post users_path, ...
+      # after_count  = User.count
+      # assert_equal before_count, after_count
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    # users/new ページに移動することを確認
+    assert_template 'users/new'
+  end
+
+  test 'invalid name' do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@example.com",
+                                         password:              "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    assert_select 'div[id=?]>ul>li','error_explanation' ,'Name can\'t be blank'
+  end
+end
