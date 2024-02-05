2d80719834259b12a56bd0d0c7a20141219aab68
nasubibocchi <matsuhito125@gmail.com>
2024-02-05 14:29:51 +0900

    fix: find argument in show controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(user_params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      # render
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
